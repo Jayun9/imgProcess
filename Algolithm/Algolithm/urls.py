6fc44c71470d3d25b041d54bb73f7af75919494b
koo <ninejy_lol@naver.com>
2020-10-28 17:25:59 +0900

    ajax 통신 연걸

--- Algolithm/Algolithm/urls.py
+++ Algolithm/Algolithm/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
+from imageprocessing import views as im_v
+from django.conf.urls.static import static
+from django.conf import settings
 
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
-    path('', include('imageprocessing.urls'))
-    
+    path('', im_v.image_process),
+    path('seg/',im_v.img_seg),
+    path('seg/export/',im_v.export) 
 ] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Algolithm/imageprocessing/views.py
+++ Algolithm/imageprocessing/views.py
@@ -5,25 +5,12 @@ from .forms import ImageUploadForm
 from django.conf import settings
 from .image_seg import segment
 import os
-from django.http import HttpResponse
+from django.http import HttpResponse, QueryDict
 import simplejson as js
 from django.views.generic import View
 
 def image_process(request):
-    return render(request, 'image_process/base.html',{})
-
-def uimage(request):
-    if request.method =='POST':
-        form = UploadImageForm(request.POST, request.FILES)
-        if form.is_valid():
-            myfile = request.FILES['image']
-            fs = FileSystemStorage()
-            filename = fs.save(myfile.name, myfile)
-            uploaded_file_url = fs.url(filename)
-            return render(request, 'image_process/uimage.html', {'form' : form , 'uploaded_file_url' : uploaded_file_url})
-    else:
-        form = UploadImageForm()
-        return render(request, 'image_process/uimage.html', {'form' : form})
+    return render(request, 'image_process/algolithm.html')
 
 class dface(View):
     def __init__(self):
@@ -55,5 +42,41 @@ class dface(View):
                         image.append(image_name)
                 return render(request, 'image_process/algolithm.html', {'post' : post, 'post_path' : self.post_path, 'image_list' : image})
     def get(self, request, *args, **kwargs):
-        return render(request, 'image_process/algolithm.html')
+        return render(request, 'imageprocess/algolithm.html')
 
+def img_seg(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            
+            imageURL = settings.MEDIA_URL + form.instance.image.name 
+            json = request.FILES['json_file']
+            segment(settings.MEDIA_ROOT_URL + imageURL, json)
+
+            result_image_path = post.image.url
+            original_name = result_image_path.split('/')[-1]
+            result_image_path = result_image_path.split('/')[:-1]
+            result_image_path = '/'.join(result_image_path)
+            image_list = os.listdir("{}/{}".format(settings.MEDIA_ROOT_URL,result_image_path))
+            image = []
+            post_path = []
+            for image_name in image_list:
+                if image_name != original_name:
+                    image_path = "{}/{}".format(result_image_path,image_name)
+                    post_path.append(image_path)
+                    image.append(image_name)
+            return render(request, 'image_process/algolithm.html', {'post' : post, 'post_path' : post_path, 'image_list' : image})
+    else:
+        form = ImageUploadForm()
+        return render(request, 'image_process/algolithm.html',{'form': form})
+
+def export(request):
+    request_dict = request.POST.dict()
+    print(request_dict)
+    query_dict = QueryDict('', mutable=True)
+    query_dict.update(request_dict)
+    print(query_dict)
+    form = ImageUploadForm()
+    return render(request, 'image_process/algolithm.html',{'form': form})
